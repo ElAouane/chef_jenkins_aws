6947f4c476b1e0a8ecd5b492241d036868431876
Hamza El Aouane <haouane@spartaglobal.com>
2020-03-11 21:35:42 +0000

    Mongo Fixed

--- cookbooks/mongo_cookbook/attributes/default.rb
+++ cookbooks/mongo_cookbook/attributes/default.rb
@@ -1,3 +1,2 @@
-default['mongodb']['install_method'] = 'mongodb-org'
-default['mongodb']['config']['mongod']['net']['port'] = 27017
-default['mongodb']['config']['mongod']['net']['bindIp'] = '0.0.0.0'
+default['mongodb']['port'] = 27017
+default['mongodb']['bind_ip'] = '0.0.0.0'
