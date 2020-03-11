b23434b748dfea0c1c08f04ab3935596961e29bc
Hamza El Aouane <haouane@spartaglobal.com>
2020-03-11 14:48:49 +0000

    Test webhook3

--- cookbooks/node_cookbook/spec/unit/recipes/default_spec.rb
+++ cookbooks/node_cookbook/spec/unit/recipes/default_spec.rb
@@ -16,7 +16,7 @@ describe 'node_cookbook::default' do
       expect { chef_run }.to_not raise_error
     end
     it 'runs apt get update' do
-      expect { chef_run }.to update_apt_update 'update_source'
+      expect { chef_run }.to update_apt_update 'update_sources'
     end
     it 'should install nginx' do
       expect(chef_run).to install_package "nginx"
